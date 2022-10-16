134d054052c1c0827b370b9e23107cc330acb83b
JCWin <jcm91186@gmail.com>
2022-10-16 09:24:29 -0700

    Mapped keys in js file and set actions on HTML elements

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
+const display = document.getElementById('display');
+const keys = document.querySelector('.keys');
+
 window.addEventListener('keydown', function(e) {
     console.log(e.keyCode);
+});
+
+keys.addEventListener('click', e => {
+    const key = e.target;
+    const action = key.dataset.action;
+    if (key.matches('.squarebutton')) { // disables the parent div from being selectable
+        if (action == null) console.log(`number key: ${key.textContent}`);
+        if (action === 'clear' || action === 'positive_negative' || action === 'percent' || action === 'divide' || action === 'multiply' || action === 'minus' || action === 'add' || action === 'equal') {
+            console.log(`execute operation: ${action}`);
+        }
+    }
 });

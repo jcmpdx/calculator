e4042f4cc4104e06614a7fc14b0286168522c88f
JCWin <jcm91186@gmail.com>
2022-10-17 14:36:20 -0700

    Created calc function, 1st and 2nd value logic on operator and equals actions. Basic functionality working. Next is to create logic for edge cases

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const add = function(a, b) {
-	return a + b;
+	return parseFloat(a) + parseFloat(b);
 };
 
 const subtract = function(a, b) {
-	return a - b;
+	return parseFloat(a) - parseFloat(b);
 };
 
 const multiply = function(a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 };
 
 const divide = function(a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 }
 
 const power = function(a, b) {
-	return a ** b;
+	return parseFloat(a) ** parseFloat(b);
 };
+
+function calc(a, operator, b) {
+    let result = 0;
+    if (operator === 'add') result = add(a, b);
+    if (operator === 'minus') result = subtract(a, b);
+    if (operator === 'multiply') result = multiply(a, b);
+    if (operator === 'divide') result = divide(a, b);
+    return result.toFixed(4);
+}
 
 const display = document.getElementById('display');
 const keys = document.querySelector('.keys');
 let afterAction = false;
+let firstValue = 0;
+let secondValue = 0;
+let operator = '';
 
 keys.addEventListener('click', e => {
     const key = e.target;
@@ -45,6 +57,14 @@ keys.addEventListener('click', e => {
         if (action === 'divide' || action === 'multiply' || action === 'minus' || action === 'add') {
             key.classList.add('selected');
             afterAction = true;
+            firstValue = display.textContent;
+            operator = action;
+            console.log(`1st value=${firstValue} and operator=${operator}`);
+        }
+        if (action === 'equal') {
+            secondValue = display.textContent;
+            console.log(`2nd value=${secondValue}`);
+            display.textContent = calc(firstValue, operator, secondValue);
         }
     }
 

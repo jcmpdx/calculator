59bf764e84dd58d6e29b2d22d0aa92efdf7acfc9
JCWin <jcm91186@gmail.com>
2022-10-16 20:11:59 -0700

    Added if else logic to JS for inputting numbers, highlighing operator keys, and resetting display to take inputs after operator keys clicked

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const power = function(a, b) {
+	return a ** b;
+};
 
 const display = document.getElementById('display');
 const keys = document.querySelector('.keys');
-
-window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
-});
+let afterAction = false;
 
 keys.addEventListener('click', e => {
     const key = e.target;
     const action = key.dataset.action;
     if (key.matches('.squarebutton')) { // disables the parent div from being selectable
-        if (action == null) console.log(`number key: ${key.textContent}`);
-        if (action === 'clear' || action === 'positive_negative' || action === 'percent' || action === 'divide' || action === 'multiply' || action === 'minus' || action === 'add' || action === 'equal') {
-            console.log(`execute operation: ${action}`);
+        // loops through all keys and removes .selected
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('selected'));
+        if (action === 'clear') {
+            display.textContent = 0;
+        }
+        if (!action) {
+            if (display.textContent === '0' || afterAction === true) {
+                display.textContent = key.textContent;
+                afterAction = false;
+            } else {
+                display.textContent += key.textContent;
+            }
+        }
+        if (action === 'decimal') {
+            display.textContent += '.';
+        }
+        if (action === 'divide' || action === 'multiply' || action === 'minus' || action === 'add') {
+            key.classList.add('selected');
+            afterAction = true;
         }
     }
+
 });
